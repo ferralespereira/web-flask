a5f9137b21e82e801a038aa46c904fdea271b0ea
javier ferrales <ferralespereira@gmail.com>
2022-06-02 16:41:34 -0400

    creating routes

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return "learning flask"
 
+@app.route('/information')
+def information():
+    return "<h1>information</h1>"
+
+@app.route('/contact')
+def contact():
+    return "<h1>contact</h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
 
